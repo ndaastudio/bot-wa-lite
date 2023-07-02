5cae8419eebaf75230e49ec285c9c7041bfb597a
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-02 15:40:52 +0700

    birthday

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ client.initialize();
 let isStart = false;
 let isTebak = false;
 client.on("message", async (message) => {
+  console.log(message.getContact());
   let sender = (await message.getContact()).pushname;
   if (isStart === false) {
     if (
